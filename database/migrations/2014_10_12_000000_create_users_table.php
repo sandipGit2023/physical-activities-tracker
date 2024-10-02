9af40cf346dea191dac2afb0b90f3fe1dfac8c42
sandip <ghodasarasandip011@gmail.com>
2024-10-02 19:27:49 +0530

    update readme

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->integer('total_points')->default(0);
             $table->timestamps();
         });
     }
